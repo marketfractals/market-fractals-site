02bab39c3f4063e1070e2e78171101ba65198ed5
Sajjad Edalatzadeh <msedalatzadeh@gmail.com>
2024-12-13 21:28:54 -0500

    landing page corrected

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Under Construction.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/company/marketfractals"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tentative launch in 2025
         </a>
       </header>
     </div>
