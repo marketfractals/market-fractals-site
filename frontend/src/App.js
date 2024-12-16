5ca55a700a74833ed0aeb2ae9631172dd69004b2
Sajjad Edalatzadeh <msedalatzadeh@gmail.com>
2024-12-15 20:12:49 -0500

    seperate front and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Under Construction.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/company/marketfractals"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tentative launch in 2025
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
